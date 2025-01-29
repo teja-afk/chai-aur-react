3a3c6cee2c4be514f1f68ab366e41cf38ffa5e7a
teja-afk <tejapoosa123@gmail.com>
2025-01-29 12:24:44 +0530

    react so far

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React | Teja</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+    return (
+      <h1>Chai aur React with vite | Teja</h1>
+  )
+}
+
+export default App
